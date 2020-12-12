cc5190ff0d5015475cf2670d352d8b0a59a79851
faizahsayyid <faizah.sayyid@gmail.com>
2020-12-12 18:53:35 -0500

    Gave values to all states
    Previously some of the states in our plotly animation had no colour, so I fixed it so that they all have colour now

--- search_for_keywords.py
+++ search_for_keywords.py
@@ -28,6 +28,10 @@ PUNCTUATION = ['.', ',', '!', '?', ';', ':', "'", '‘', '’', '“', '``', "''
 OTHER = ['@', 'https', "'s", 'u', '...', '..', '%', '$', '—', '–', '\u2066', "'ve", "'re", "'m",
          "n't", 'the…']
 
+STATES = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+          'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+          'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
 
 # ==================================================================================================
 # Functions for getting the data points for our plotly graph (for hashtags)
@@ -69,16 +73,19 @@ def date_state_to_hashtag_pop(tweets: List[Tweet], hashtag: str) -> Dict[Tuple[s
     """Return a dictionary where the keys are a tuples of dates and states (dates are strings),
     and the corresponding values is the popularity of the given hashtag in that state at that time
     """
-    d_s_to_hashtag_pop = {}
+    date_to_t = date_to_tweet(tweets)
+    d_s_to_hashtag_pop = {(date, state): 0 for date in date_to_t for state in STATES}
 
-    for tweet in tweets:
-        date_state = (tweet.date, tweet.state)
+    for date in date_to_t:
 
-        if hashtag in tweet.hashtags:
-            if date_state in d_s_to_hashtag_pop:
-                d_s_to_hashtag_pop[date_state] += 1
-            else:
-                d_s_to_hashtag_pop[date_state] = 1
+        for state in STATES:
+            date_state = (date, state)
+
+            for tweet in date_to_t[date]:
+
+                if hashtag in tweet.hashtags and (tweet.state == state):
+                    if date_state in d_s_to_hashtag_pop:
+                        d_s_to_hashtag_pop[date_state] += 1
 
     return d_s_to_hashtag_pop
 
@@ -124,14 +131,19 @@ def date_state_to_phrase_occurrences(tweets: List[Tweet], search_phrase: tuple) 
     and the corresponding values are the number of times the search_phrase occurs in every tweet
     that was tweeted on that date and in that state
     """
-    d_s_to_occs_so_far = {}
 
-    for tweet in tweets:
-        date_state = (tweet.date, tweet.state)
-        if date_state in d_s_to_occs_so_far:
-            d_s_to_occs_so_far[date_state] += phrase_occurences_in_tweet(tweet, search_phrase)
-        elif date_state not in d_s_to_occs_so_far:
-            d_s_to_occs_so_far[date_state] = phrase_occurences_in_tweet(tweet, search_phrase)
+    date_to_t = date_to_tweet(tweets)
+
+    d_s_to_occs_so_far = {(date, state): 0 for date in date_to_t for state in STATES}
+
+    for date in date_to_t:
+
+        for state in STATES:
+            date_state = (date, state)
+
+            for tweet in date_to_t[date]:
+                if date_state in d_s_to_occs_so_far and (tweet.state == state):
+                    d_s_to_occs_so_far[date_state] += phrase_occurences_in_tweet(tweet, search_phrase)
 
     return d_s_to_occs_so_far
 
@@ -140,6 +152,21 @@ def date_state_to_phrase_occurrences(tweets: List[Tweet], search_phrase: tuple) 
 # Helper function for converting a Dict[Tuple[str, str], int] from date_state_to_phrase_occurrences
 # and date_state_to_hashtag_pop into data points for plotly
 # ==================================================================================================
+
+def date_to_tweet(tweets: List[Tweet]) -> Dict[str, List[Tweet]]:
+    """Return a dictionary where the keys are all the dates that a tweet occurred within the
+    given list of tweets, and the values are the tweets that occured on that day"""
+
+    d_to_t_so_far = {}
+
+    for tweet in tweets:
+        if tweet.date in d_to_t_so_far:
+            d_to_t_so_far[tweet.date].append(tweet)
+        else:
+            d_to_t_so_far[tweet.date] = [tweet]
+
+    return d_to_t_so_far
+
 
 def data_points(data_dict: Dict[Tuple[str, str], int]) -> Tuple[List[str], List[str], List[int]]:
     """Return a tuple of lists for the given data that corresponds to format plotly needs for a
